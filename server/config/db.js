2cbeb37c98217e279dd899295a4ab40d36780e39
Ticha23 <mutwirigachanja1@gmail.com>
2025-07-25 22:01:32 +0300

    Backend going live succesfully

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    console.log('👉 MONGO_URI_PRODUCTION:', process.env.MONGO_URI_PRODUCTION);
     await mongoose.connect(process.env.MONGO_URI_PRODUCTION);
     console.log('MongoDB connected');
   } catch (error) {
