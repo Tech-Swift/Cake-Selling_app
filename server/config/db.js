9d134a100c2f3bf49d7ab743b2cb4a023ca24c46
Ticha23 <mutwirigachanja1@gmail.com>
2025-07-25 21:10:47 +0300

    Debugging

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    console.log('👉 MONGO_URI_PRODUCTION:', process.env.MONGO_URI_PRODUCTION);
+    await mongoose.connect(process.env.MONGO_URI_PRODUCTION);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
