e1938d14231098e2f48479950a9f4bd93749ed35
Ticha23 <mutwirigachanja1@gmail.com>
2025-07-28 19:16:28 +0300

    Check the connection between the server and client

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://cake-selling-app.onrender.com/",
+  baseURL: "https://cake-selling-app.onrender.com/api", 
   withCredentials: true,
 });
 
